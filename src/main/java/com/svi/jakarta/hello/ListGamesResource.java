ddb1337c483094138ce9e8acef82052c5261def5
glenannang <angelaannang8@gmail.com>
2026-09-01 16:19:16 +0800

    feat: read player game records

--- src/main/java/com/svi/jakarta/hello/ListGamesResource.java
+++ src/main/java/com/svi/jakarta/hello/ListGamesResource.java
@@ -7,7 +7,9 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.File;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 @Path("list-games")
 public class ListGamesResource {
@@ -24,8 +26,23 @@ public class ListGamesResource {
             return Response.status(402).entity("{\"msg\":\"Record not found\"}").build();
         }
 
+        try(BufferedReader reader = new BufferedReader(new FileReader(playerFile))){
+            String line;
+            String games= "";
 
-        return Response.status(200).entity("{\"msg\":\"Records found\"}").build();
+            while((line = reader.readLine()) != null){
+                //System.out.println(line);
+                games += line + " ";
+            }
+
+            return Response.status(200).entity("{\"games\":\"" + games + "\"}").build();
+
+        }catch(IOException e){
+            return Response.status(500).entity("{\"msg\":\"The server ran into an unexpected exception.\"}").build();
+        }
+
+
+
     }
 
 }
