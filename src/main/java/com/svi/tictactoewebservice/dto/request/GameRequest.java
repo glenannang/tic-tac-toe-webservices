171053ee36fe4dbdc89b034517a65683e636efe7
glenannang <angelaannang8@gmail.com>
2026-09-04 11:11:16 +0800

    feat: add request body for get game details endpoint

---
+++ src/main/java/com/svi/tictactoewebservice/dto/request/GameRequest.java
@@ -0,0 +1,16 @@
+package com.svi.tictactoewebservice.dto.request;
+
+public class GameRequest {
+    private String gameId;
+
+    public String getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(String gameId) {
+        this.gameId = gameId;
+    }
+
+}
+
+
