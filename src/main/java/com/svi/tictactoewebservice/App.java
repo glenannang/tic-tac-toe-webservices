840503a17208ca405cd6d0b789938787d0a5d7a0
glenannang <angelaannang8@gmail.com>
2026-09-07 03:14:38 +0800

    refactor: change application path from rest to api

--- src/main/java/com/svi/tictactoewebservice/App.java
+++ src/main/java/com/svi/tictactoewebservice/App.java
@@ -3,7 +3,7 @@ package com.svi.tictactoewebservice;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.ApplicationPath;
 
-@ApplicationPath("rest")
+@ApplicationPath("api")
 public class App extends Application {
     // Needed to enable Jakarta REST and specify path.
 }
